6fb577b1de96f6fe5fe1117224ebf138a4bf0fbe
MatimaMk <matima547@gmail.com>
2025-05-01 11:36:46 +0200

    updated service

--- aspnet-core/src/visionMath.Application/Services/ContentServices/Dtos/ContentDto.cs
+++ aspnet-core/src/visionMath.Application/Services/ContentServices/Dtos/ContentDto.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
 using visionMath.Domain.Resources;
